49492bc4bca78e2334da667f578318a575b796ed
isoul87 <atbrxnk@gmail.com>
2025-09-01 13:44:12 +0700

    add perfomance

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,32 @@
 
     AOS.init();
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("coomfstwo-cache").then((cache) => {
+      return cache.addAll([
+        "/",              // halaman utama
+        "/index.html",    // file html
+        "/src/input.css",     // css
+        "/dist/script.js",     // js
+        "/src/img" // gambar
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/dist/script.js")
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     window.onscroll = function () {
     const header = document.querySelector('header');
