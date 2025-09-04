f764be41c8ae4158974ad3b38ea490f7d6caefba
isoul87 <atbrxnk@gmail.com>
2025-09-04 11:34:26 +0700

    a

--- dist/script.js
+++ dist/script.js
@@ -2,7 +2,7 @@
       const loaderElement = document.getElementById('loader');
       const mainContentElement = document.getElementById('main-content');
 
-      window.addEventListener('DOMContentLoaded', function() {
+      window.addEventListener('load', function() {
         loaderElement.classList.add('hidden'); // Sembunyikan loader
         mainContentElement.classList.remove('hidden'); // Tampilkan konten
       });
